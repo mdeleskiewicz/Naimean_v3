e00de655d8e7849caeb95153979a01e60d6ab9c1
naimean <naimean@hotmail.com>
2026-06-30 23:36:31 -0400

    Update appRuntime.js

--- public/assets/js/appRuntime.js
+++ public/assets/js/appRuntime.js
@@ -13,6 +13,7 @@ import './ui/overlays.js';
 import './systems/leftMonitorCards.js';
 import './systems/cloudflare.js';
 import './cards/cardOrchestrator.js';
+import { bootstrapMaestro } from './maestro/maestro.js';
 import { observePerformanceMetrics } from './systems/performance.js';
 import { bootstrapScene } from './systems/scene.js';
 
