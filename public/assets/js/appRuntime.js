e17d64e28d56087009d68a96a91c87838420b54d
naimean <naimean@hotmail.com>
2026-06-30 23:37:39 -0400

    Add bootstrapMaestro to app runtime initialization

--- public/assets/js/appRuntime.js
+++ public/assets/js/appRuntime.js
@@ -24,6 +24,7 @@ export function bootstrapApp() {
     initDomRefs();
     observePerformanceMetrics();
     bootstrapScene();
+    bootstrapMaestro();
 
     console.info('[Naimean] App runtime bootstrapped.');
   } catch (error) {
